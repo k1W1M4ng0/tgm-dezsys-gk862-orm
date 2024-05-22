086cb3b29212c60770c16e45b4b3f1cc3442e832
Simon Gao <simon.shubin@gmail.com>
2024-05-22 17:41:08 +0200

    ✨ Added methods for collecting data

--- src/main/java/syt/springorm/MainController.java
+++ src/main/java/syt/springorm/MainController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +15,7 @@ import syt.springorm.data.Product;
 import syt.springorm.data.WarehouseData;
 
 @Controller
+@CrossOrigin
 public class MainController {
     // autowire the generated userRepository bean to this
     @Autowired
@@ -71,6 +75,55 @@ public class MainController {
         whRepo.save(new WarehouseData());
 
         return "saved";
+    }
+
+    @RequestMapping("/id/warehouse/{id}")
+    public @ResponseBody WarehouseData getWarehouseById(@PathVariable int id) {
+        return whRepo.findById(id).orElse(null);
+    }
+
+    @RequestMapping("/id/warehouse/{warehouseID}/product/{productID}")
+    public @ResponseBody Product getSingleProductFromWarehouse(@PathVariable int warehouseID, @PathVariable int productID) {
+        return pRepo.findById(productID)
+            .filter(p -> p.getWarehouse().getWarehouseID() == warehouseID)
+            .orElse(null);
+    }
+
+    @RequestMapping("/update/warehouse/{id}")
+    public @ResponseBody UpdateRequest updateWarehouse(@PathVariable int id, @RequestBody UpdateRequest req) {
+        var newObject = whRepo.findById(id).orElseThrow();
+
+        if(req.warehouseApplicationID.length() > 0) {
+            newObject.setWarehouseApplicationID(req.warehouseApplicationID);
+        }
+        if(req.warehouseName.length() > 0) {
+            newObject.setWarehouseName(req.warehouseName);
+        }
+        if(req.warehouseAddress.length() > 0) {
+            newObject.setWarehouseAddress(req.warehouseAddress);
+        }
+        if(req.warehousePostalCode.length() > 0) {
+            newObject.setWarehousePostalCode(req.warehousePostalCode);
+        }
+        if(req.warehouseCity.length() > 0) {
+            newObject.setWarehouseCity(req.warehouseCity);
+        }
+        if(req.warehouseCountry.length() > 0) {
+            newObject.setWarehouseCountry(req.warehouseCountry);
+        }
+
+        whRepo.save(newObject);
+
+        return req;
+    }
+
+    static class UpdateRequest {
+        public String warehouseApplicationID;
+        public String warehouseName;
+        public String warehouseAddress;
+        public String warehousePostalCode;
+        public String warehouseCity;
+        public String warehouseCountry;
     }
 
 }
